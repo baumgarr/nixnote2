d03d290c722fef3222b0d51b9fdafb7dd860ef99
Randy Baumgarte <randy@fbn.cx>
2013-02-22 10:04:07 -0500

    Updated licensing info.

--- settings/accountsmanager.cpp
+++ settings/accountsmanager.cpp
@@ -1,3 +1,22 @@
+/*********************************************************************************
+NixNote - An open-source client for the Evernote service.
+Copyright (C) 2013 Randy Baumgarte
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+***********************************************************************************/
+
 #include "accountsmanager.h"
 #include "global.h"
 
@@ -40,11 +59,6 @@ AccountsManager::AccountsManager(int id, QObject *parent) :
             i=nodes.size();
         }
     }
-    setOAuthToken("hhhhhh");
-    addId(0,"test");
-    setOAuthToken("bbb");
-    removeId(2);
-    removeId(1);
 }
 
 
