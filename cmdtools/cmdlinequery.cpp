8c09f4c63bcf96c1c9859c7a7b23b7c1e8a320c9
Randy Baumgarte <randy@fbn.cx>
2015-12-03 07:28:55 -0500

    Initialize outputFile to remove compile warning.

--- cmdtools/cmdlinequery.cpp
+++ cmdtools/cmdlinequery.cpp
@@ -47,7 +47,7 @@ void CmdLineQuery::write(QList<qint32> lids, QString filename) {
     QString delimiter = "|";
     if (this->delimiter != "")
         delimiter = this->delimiter;
-    QFile *outputFile;
+    QFile *outputFile = NULL;
     if (filename != "") {
         outputFile = new QFile(filename);
         stdout=false;
