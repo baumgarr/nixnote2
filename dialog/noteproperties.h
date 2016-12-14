25f4b0b8ee5f8197b3feaaa718ff7fbca5836370
Randy Baumgarte <randy@fbn.cx>
2016-12-14 18:40:40 -0500

    Add missing QOBJECT macro.

--- dialog/noteproperties.h
+++ dialog/noteproperties.h
@@ -28,6 +28,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 class NoteProperties : public QDialog
 {
+    Q_OBJECT
 private:
 public:
     NoteProperties();
--- dialog/shortcutdialog.h
+++ dialog/shortcutdialog.h
@@ -28,6 +28,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 class ShortcutDialog : public QDialog
 {
+    Q_OBJECT
 private:
     QPushButton *okButton;
     QTableWidget *tableWidget;
